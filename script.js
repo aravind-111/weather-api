65663675a3a072d8d678c81fbab6a5c967a6faa0
Aravindan-111 <aravindhan11195@gmail.com>
2021-08-31 15:27:12 +0530

    try catch added

--- script.js
+++ script.js
@@ -5,29 +5,35 @@ let nam = document.querySelector('.name');
 let date = document.querySelector('.date');
 let search = document.querySelector('.search');
 
+// city.value = 'chennai';
+
 search.addEventListener('click', async function () {
   let url =
     'https://api.openweathermap.org/data/2.5/weather?q=' +
     city.value +
     '&appid=89a866485aecd671d20a63958e48c1c9';
-  let data_dummy = await fetch(url);
-  let data = await data_dummy.json();
-  let city_name = await data.name;
-  console.log(city_name);
-  let city_temp = await Math.floor(data.main.temp);
-  let city_cloud = await data.weather[0].main;
-  let date_time = new Date().toDateString();
-  nam.innerHTML = `${city_name}`;
-  temp.innerHTML = `${city_temp - 273} &#176; C`;
-  date.innerHTML = `${date_time}`;
-  if (city_cloud == 'Clouds') {
-    src = '/assets/cloudy.png';
-  } else if (city_cloud == 'Rain') {
-    src = '/assets/rainy.png';
-  } else {
-    src = '/assets/sunny.png';
+  try {
+    let data_dummy = await fetch(url);
+    let data = await data_dummy.json();
+    let city_name = await data.name;
+    console.log(city_name);
+    let city_temp = await Math.floor(data.main.temp);
+    let city_cloud = await data.weather[0].main;
+    let date_time = new Date().toDateString();
+    nam.innerHTML = `${city_name}`;
+    temp.innerHTML = `${city_temp - 273} &#176; C`;
+    date.innerHTML = `${date_time}`;
+    if (city_cloud == 'Clouds') {
+      src = '/assets/cloudy.png';
+    } else if (city_cloud == 'Rain') {
+      src = '/assets/rainy.png';
+    } else {
+      src = '/assets/sunny.png';
+    }
+    cloud.innerHTML = `<img class="cloudy" src=${src}>`;
+  } catch (err) {
+    alert('Enter the Correct City Name');
   }
-  cloud.innerHTML = `<img class="cloudy" src=${src}>`;
 
   console.log(city_name);
   console.log(city_temp);
